129d0d4394c06260b4fc81479a3f05d25a268a04
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-10 08:26:40 +0300

    commit tree b22ec381adcd43ecc1b58a93337cee9f1bb4a77e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+
+
+function superbowlWin(records){
+    let rec = records.find( function (record) {
+        return record.result == "W"
+    })
+    console.log('rec', rec)
+    //get the year
+    if (rec) {
+        return rec.year 
+    } else {
+        return undefined
+    }
+}
+console.log(superbowlWin(record));
